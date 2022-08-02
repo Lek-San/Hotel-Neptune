9cd92888ea853106afc26c4f4b1f3ca566d092b4
Xavier Chassang <chassang.xavier@gmail.com>
2022-08-02 12:06:10 +0200

    Test connexion DB

--- src/main/java/fr/simplon/neptunians/Hotel_Neptune/PrimaryController.java
+++ src/main/java/fr/simplon/neptunians/Hotel_Neptune/PrimaryController.java
@@ -1,10 +1,35 @@
 package fr.simplon.neptunians.Hotel_Neptune;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class PrimaryController {
+	@FXML
+	private Label testConnect;
 
+	public void connectButton(ActionEvent event) {
+		DatabaseConnection connectNow = new DatabaseConnection();
+		Connection connectDB = connectNow.getConnection();
+				
+		String connectQuery = "SELECT nom FROM commune";
+		
+		try {
+			Statement statement = connectDB.createStatement();
+			ResultSet queryOutput = statement.executeQuery(connectQuery);
+			
+			while (queryOutput.next()) {
+				testConnect.setText(queryOutput.getString("nom"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module fr.simplon.neptunians.Hotel_Neptune {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+	requires javafx.base;
 
     opens fr.simplon.neptunians.Hotel_Neptune to javafx.fxml;
     exports fr.simplon.neptunians.Hotel_Neptune;
