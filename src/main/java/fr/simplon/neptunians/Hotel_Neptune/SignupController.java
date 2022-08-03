202b9cde2bc9badc601cb08b5249982146f6bbb4
Lek-San <leksan.webdev@gmail.com>
2022-08-03 13:01:59 +0200

    Alex:small changes

--- src/main/java/fr/simplon/neptunians/Hotel_Neptune/SignupController.java
+++ src/main/java/fr/simplon/neptunians/Hotel_Neptune/SignupController.java
@@ -1,5 +1,32 @@
 package fr.simplon.neptunians.Hotel_Neptune;
 
-public class SignupController extends BackToController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
+public class SignupController extends BackToController {
+	
+	@FXML
+	private TextField fieldFirstName;
+	@FXML
+	private TextField fieldName;
+	@FXML
+	private TextField fieldDay;
+	@FXML
+	private TextField fieldMonth;
+	@FXML
+	private TextField fieldYear;
+	@FXML
+	private TextField fieldEmail;
+	@FXML
+	private PasswordField fieldPass;
+	@FXML
+	private TextField fieldcomfPass;
+	@FXML
+	private Button btnSubmitCreateAccount;
+	
+	
+	
+	
 }
