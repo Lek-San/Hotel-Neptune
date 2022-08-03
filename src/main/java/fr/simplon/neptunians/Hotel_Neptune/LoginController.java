dab427e06f2f0dc94d3ced04bc5633d2c10344e8
Lek-San <leksan.webdev@gmail.com>
2022-08-03 17:00:19 +0200

    Alex: ????

--- src/main/java/fr/simplon/neptunians/Hotel_Neptune/LoginController.java
+++ src/main/java/fr/simplon/neptunians/Hotel_Neptune/LoginController.java
@@ -1,5 +1,19 @@
 package fr.simplon.neptunians.Hotel_Neptune;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginController extends BackToController {
+	
+	@FXML
+	private TextField fieldLoginLogin;
+	@FXML
+	private PasswordField fieldPassLogin;
+	@FXML
+	private Button btnSubmitLogin;
+	@FXML
+	private Button btnSignupLogin;
 
 }
