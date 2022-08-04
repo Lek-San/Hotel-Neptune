a30ed695146b1b67e145c34e8b1d83b8d4c15e33
Xavier Chassang <chassang.xavier@gmail.com>
2022-08-04 11:52:00 +0200

    Xavier: Ajout commentaires connexion

--- src/main/java/fr/simplon/neptunians/Hotel_Neptune/BackofficeController.java
+++ src/main/java/fr/simplon/neptunians/Hotel_Neptune/BackofficeController.java
@@ -21,13 +21,17 @@ public class BackofficeController extends BackToController {
 	@FXML
 	public TableColumn<Type, String> col_name_type;
 	
+	// Apparement, pour créer des lignes dans un tableau avec javafx on doit utiliser la méthode setItems qui prend comme paramètre une ObservableList et c'est tout.
 	ObservableList<Type> typeObsList = FXCollections.observableArrayList();
 	
 	public void displayTypes(ActionEvent event) {
+		
+		// On instancie la classe qui gère la connexion (classe que j'ai créé, pas le framework) et on appelle la méhode pour stocker le résultat de cette connexion dans connectDB
 		DatabaseConnection connectNow = new DatabaseConnection();
 		Connection connectDB = connectNow.getConnection();
 		
 		try {
+			// ResultSet c'est le résultat, similaire à un tableau, de ma requête sql. Pour l'obtenir on doit créer un statement car c'est dans l'instance de cette classe que se trouve la méthode permettant de faire une requête sql.
 			ResultSet typeSet = connectDB.createStatement().executeQuery("SELECT * FROM type");
 			
 			while (typeSet.next()) {
@@ -43,5 +47,6 @@ public class BackofficeController extends BackToController {
 		col_name_type.setCellValueFactory(new PropertyValueFactory<Type, String>("type_name"));
 		
 		display_table.setItems(typeObsList);
+		
 	}
 }
--- src/main/java/fr/simplon/neptunians/Hotel_Neptune/DatabaseConnection.java
+++ src/main/java/fr/simplon/neptunians/Hotel_Neptune/DatabaseConnection.java
@@ -3,17 +3,24 @@ package fr.simplon.neptunians.Hotel_Neptune;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+// Cette classe va retourner permettre de retourner un objet de type Connection lequel représente la connexion à la base de données.
 public class DatabaseConnection {
+	
 	public Connection databaseLink;
 	
+	// Un simple getter, unique méthode de ma classe.
 	public Connection getConnection() {
+		// Les informations de connexion de ma base de données.
 		String databaseName = "hotel_neptune";
 		String databaseUser = "root";
 		String databasePassword = "";
 		String url = "jdbc:mysql://localhost/" + databaseName;
 		
+		// Try catch obligatoire pour gérer les échecs de connection à mysql.
 		try {
+			// Je crois que c'est un truc de sécurité...
 			Class.forName("com.mysql.cj.jdbc.Driver");
+			// C'est la méthode getConnection de l'interface DriverManager qui va vraiment gérer la co avec la base de données.
 			databaseLink = DriverManager.getConnection(url, databaseUser, databasePassword);
 		} catch (Exception e) {
 			e.printStackTrace();
